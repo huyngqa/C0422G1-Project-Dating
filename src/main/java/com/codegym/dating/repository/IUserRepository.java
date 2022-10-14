39dce6aa8c9ba36c12b1734d5573df0176b33b72
Thanh Trang <you@example.com>
2022-10-14 19:03:59 +0700

    bt

--- src/main/java/com/codegym/dating/repository/IUserRepository.java
+++ src/main/java/com/codegym/dating/repository/IUserRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface IUserRepository extends JpaRepository<User, Integer> {
-    @Query(value = "SELECT user.`name`,user.coin,user.gender,user. address,user.job,user .avatar FROM user where  user.`name` like :name", countQuery = "select count(*) from user", nativeQuery = true)
-    Page<UserDto> getAllSearchPage( Pageable pageable,@Param("name") String name);
+    @Query(value = "SELECT user.`name`,user.coin,user.gender,user. address,user.job,user .avatar FROM user where  user.`name` like :name",
+            countQuery = "select count(*) from (SELECT user.`name`,user.coin,user.gender,user. address,user.job,user .avatar FROM user where  user.`name` like :name)as pageCount",
+            nativeQuery = true)
+    Page<UserDto> getAllSearchPage(Pageable pageable, @Param("name") String name);
 }
