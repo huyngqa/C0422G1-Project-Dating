c9b905acc053ac963721a05e342c3a40ff4bfbfb
Thanh Trang <you@example.com>
2022-10-20 09:39:52 +0700

    bt

--- src/main/java/com/codegym/dating/service/IUserService.java
+++ src/main/java/com/codegym/dating/service/IUserService.java
@@ -5,7 +5,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IUserService {
-
-
     Page<UserDto> findAllSearchPage(Pageable pageable, String name);
 }
